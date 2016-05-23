d56fa4f1e9366e0009e3c266135f52a16bc28175
mfahem <mfahem@1c1b42fb-39bf-43e7-89c1-6f2afbe02e2a>
2016-05-23 20:18:46 +0000

    Status des matchs mis à jours (il manque la gestion des prolongations)

--- src/com/barbyBet/tools/WebServiceUtil.java
+++ src/com/barbyBet/tools/WebServiceUtil.java
@@ -6,20 +6,31 @@ public class WebServiceUtil {
 		
 	}
 	
-	public static int createStatus(String time) {
+	public static int createStatus(String webServiceStatus) {
 		int status = MatchStatus.NOT_STARTED;
 		
-		if(time != null) {
-			if("Finished".equals(time)) {
+		if(webServiceStatus != null) {
+			if("Finished".equals(webServiceStatus)) {
 				status = MatchStatus.ENDED;
-			} else if("Postponed".equals(time)) {
+			} else if("Postponed".equals(webServiceStatus)) {
 				status = MatchStatus.NOT_STARTED;
 			} else {
 				// TODO 
-				if("Halftime".equals(time)) {
+				if("Halftime".equals(webServiceStatus)) {
 					status = MatchStatus.HALFTIME;
 				} else {
-					status = MatchStatus.FIRST_HALF;
+					webServiceStatus.replace("'", "");
+					Long minuteOfMatch = Long.parseLong(webServiceStatus);
+					Long firstHalfMinuteLimit = 45L;
+					Long secondHalfMinuteLimit = 90L;
+					
+					if(minuteOfMatch > 0 && minuteOfMatch <= firstHalfMinuteLimit) {
+						status = MatchStatus.FIRST_HALF;
+					} else if(minuteOfMatch > firstHalfMinuteLimit && minuteOfMatch <= secondHalfMinuteLimit) {
+						status = MatchStatus.SECOND_HALF;
+					} else {
+						status = MatchStatus.OVERTIME;
+					}
 				}
 			}
 		}
